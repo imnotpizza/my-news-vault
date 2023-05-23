eea21f938e4b29bc2c4e450e59c325160e3fa63c
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-05-23 16:56:27 +0900

    #17 Feat: og용 thumbnail 추가

--- src/components/common/Meta/index.tsx
+++ src/components/common/Meta/index.tsx
@@ -5,16 +5,21 @@ import React from 'react';
  * meta tag, seo 관련 설정
  */
 
-const Meta = ({ title, description, url, image }) => {
+const Meta = ({ title, description, url }) => {
   return (
     <Head>
       <title>{title || 'I Love News'}</title>
-      <meta name="description" content={description || '쉽고 빠른 뉴스검색은 역시 I Love News! 카테고리별로 뉴스 검색, 뉴스 스크랩을 제공합니다.'} />
+      <meta
+        name="description"
+        content={
+          description || '쉽고 빠른 뉴스검색은 역시 I Love News! 카테고리별로 뉴스 검색, 뉴스 스크랩을 제공합니다.'
+        }
+      />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta property="og:title" content={title || 'I Love News'} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url || 'https://ilovenews.com'} />
-      <meta property="og:image" content={ogImage} />
+      <meta property="og:image" content={'../../../public/ilovenews-og.png'} />
       <meta property="og:article:author" content="정리습관" />
     </Head>
   );
