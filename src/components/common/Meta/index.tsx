e349b0268d9a684835ee70606b228f80c04e44fb
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-05-23 19:25:14 +0900

    #17 Chore: google dns 관련 meta태그 추가

--- src/components/common/Meta/index.tsx
+++ src/components/common/Meta/index.tsx
@@ -12,7 +12,8 @@ const Meta = ({ title, description, url }) => {
       <meta
         name="description"
         content={
-          description || '쉽고 빠른 뉴스검색은 역시 I Love News! 카테고리별로 뉴스 검색, 뉴스 스크랩을 제공합니다.'
+          description ||
+          '쉽고 빠른 뉴스검색은 역시 I Love News! 카테고리별로 뉴스 검색, 뉴스 스크랩을 제공합니다.'
         }
       />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -21,6 +22,7 @@ const Meta = ({ title, description, url }) => {
       <meta property="og:url" content={url || 'https://ilovenews.com'} />
       <meta property="og:image" content={'../../../public/ilovenews-og.png'} />
       <meta property="og:article:author" content="ilovenews" />
+      <meta google-site-verification="P6j1qmKWLdH1ySNcOJiN0Ia5aJpv-V2rW2DExEhpH0k" />
     </Head>
   );
 };
