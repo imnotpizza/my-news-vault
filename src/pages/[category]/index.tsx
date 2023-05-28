14a821b85bd9a5b7b9e133e8fea2d5ddd42bcd1f
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-05-28 17:47:40 +0900

    Feat: 카테고리별로 title 동적 적용

--- src/pages/[category]/index.tsx
+++ src/pages/[category]/index.tsx
@@ -1,8 +1,25 @@
+import Meta from '@/components/common/Meta';
 import NewsSearchPage from '@/components/search/NewsSearchPage';
+import { GetServerSideProps } from 'next';
 import React from 'react';
 
-const NewsSearchWithCategory = () => {
-  return <NewsSearchPage category={'All'} />;
+const NewsSearchWithCategory = ({ category }) => {
+  return (
+    <>
+      <Meta title={`ILoveNews-${category}`} />
+      <NewsSearchPage category={'All'} />
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req } = context;
+  const { category } = context.params;
+  return {
+    props: {
+      category,
+    },
+  };
 };
 
 export default NewsSearchWithCategory;
