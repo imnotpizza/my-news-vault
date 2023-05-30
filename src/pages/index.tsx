82f394af5ad0a678f6a37a5b570ae5223425be2e
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-05-30 20:15:51 +0900

    #20 Feat: getServerSideProps 작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Meta from '@/components/common/Meta';
 import NewsSearchPage from '@/components/search/NewsSearchPage';
 import React from 'react';
 import { random } from 'lodash-es';
+import { GetServerSideProps } from 'next';
+import { admin } from '@/firebase/admin';
 
 const NewsSearch = ({ keyword }) => {
   return (
@@ -10,6 +12,36 @@ const NewsSearch = ({ keyword }) => {
       <NewsSearchPage category={'All'} />
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const { token } = context.req.cookies;
+
+    if (token) {
+      const verifiedToken = await admin.auth().verifyIdToken(token);
+      const { uid, email } = verifiedToken;
+      return {
+        props: {
+          status: true,
+          uid,
+          email,
+        },
+      };
+    } else {
+      return {
+        props: {
+          status: false,
+        },
+      };
+    }
+  } catch (e) {
+    return {
+      props: {
+        status: false,
+      },
+    };
+  }
 };
 
 export default NewsSearch;
