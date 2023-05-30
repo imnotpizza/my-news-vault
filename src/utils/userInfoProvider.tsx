309221aa81fb8cf5945e43bd64236e404cae2f3f
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-05-30 23:14:55 +0900

    #20 Feat: 기존 로그인 상태 변경로직 제거

--- src/utils/userInfoProvider.tsx
+++ src/utils/userInfoProvider.tsx
@@ -19,25 +19,6 @@ const userInfoContext = React.createContext<IUserInfoContext>({
 const UserInfoProvider = ({ children }: any) => {
   const [userInfo, setUserInfo] = React.useState<IUserInfoContext['userInfo']>(null);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        // 로그인 상태
-        console.log('로그인 상태:', user);
-        const { displayName, email, photoURL } = user;
-        await fetchScrappedNewsList(email);
-        setUserInfo((prev) => {
-          return { displayName, email, photoURL } as TUserInfo;
-        });
-      } else {
-        // 로그아웃 상태
-        console.log('로그아웃 상태');
-        // 로그아웃 상태에 따른 처리 로직 수행 가능
-        setUserInfo((prev) => null);
-      }
-    });
-  }, []);
-
   return (
     <userInfoContext.Provider value={{ userInfo, setUserInfo, isSignin: Boolean(userInfo) }}>
       {children}
