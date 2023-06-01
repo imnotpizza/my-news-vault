d66f25122a32a89f385b89aa7df95bdbdf96c064
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-06-01 23:17:54 +0900

    #25 Feat: 메타태그 최신상태로 변경

---
+++ src/constants/appinfo.ts
@@ -0,0 +1,6 @@
+export default {
+  app_name: 'MYNEWSVAULT',
+  app_desc: '신속한 뉴스 검색과 편리한 스크랩 기능을 갖춘 앱으로 최신 뉴스를 손쉽게 관리하세요.',
+  app_url: 'https://ilovenews.vercel.app',
+  author: 'imnotpizza',
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 // Toast ui duration
 export const TOAST_DURATION = 3000;
+// 앱 정식 이름
+export const APP_NAME = 'MYNEWSVAULT';
