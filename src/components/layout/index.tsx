4c576b3835ada049597aceed548d950207a55078
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-06-08 00:29:44 +0900

    #27 Feat: layout 스타일 추가

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,28 @@
 import Header from '@/components/header/Header';
+import styled from 'styled-components';
+
+const LayoutContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BodyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const RootLayout = ({ children }) => {
   return (
-    <>
+    <LayoutContainer>
       <Header />
-      {children}
-    </>
+      <BodyContainer>{children}</BodyContainer>
+    </LayoutContainer>
   );
 };
 
