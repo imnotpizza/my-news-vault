35d9c948a9a27f89e5e125ea8503d23d5290972f
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-06-07 23:16:47 +0900

    #27 Feat: Palette 색상 추가

--- src/styles/palette.ts
+++ src/styles/palette.ts
@@ -1,3 +1,21 @@
-const Palette = {};
+const Palette = {
+  Background: '#FAFAFA',
+  White: '#FFFFFF',
+  Navy: {
+    Default: '#1A2254',
+    Navy_80: '#484E76',
+    Navy_40: '#A3A7BB',
+    Navy_10: '#E8E9EE',
+  },
+  Blue: {
+    Default: '#112EBE',
+    Blue_M: '#B5BEED',
+    Blue_L: '#E7EAF9',
+  },
+  Error: {
+    Default: '#C10606',
+    Error_L: '#FFEFEF',
+  },
+};
 
 export default Palette;
