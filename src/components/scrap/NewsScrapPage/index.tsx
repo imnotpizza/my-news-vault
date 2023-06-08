1545d81ff45435659fdc0ed1853f5f2fa8d094b2
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-06-08 15:52:05 +0900

    #27 Feat: styled 추가

--- src/components/scrap/NewsScrapPage/index.tsx
+++ src/components/scrap/NewsScrapPage/index.tsx
@@ -3,15 +3,39 @@ import useScrappedNewsList from '@/queries/useScrappedNewsList';
 import NewsItemList from '@/components/common/NewsCardList';
 import React, { useContext } from 'react';
 import { userInfoContext } from '@/utils/userInfoProvider';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .scrap-results {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 52px;
+  }
+`;
 
 const NewsScrapPage = () => {
   const { userInfo } = useContext(userInfoContext);
   const queryStates = useScrappedNewsList({ userId: userInfo?.email });
 
   return (
-    <QueryStateWrapper queryStates={queryStates}>
-      <NewsItemList newsItems={queryStates.data} />
-    </QueryStateWrapper>
+    <Container>
+      <div className='scrap-results'>
+      <QueryStateWrapper
+        isLoading={queryStates.isLoading}
+        isError={queryStates.isError}
+        isEmpty={queryStates.data.length === 0}
+      >
+        <NewsItemList newsItems={queryStates.data} />
+      </QueryStateWrapper>
+      </div>
+    </Container>
   );
 };
 
