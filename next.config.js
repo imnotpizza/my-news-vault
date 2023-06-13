c29dde838b2658fb68658fcfd68eba4e045f415f
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-06-13 23:07:06 +0900

    Chore: analyzer 커맨드수정+env 설정제거

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_BING_API_BASEURL: process.env.NEXT_PUBLIC_BING_API_BASEURL,
-    NEXT_PUBLIC_BING_API_KEY: process.env.NEXT_PUBLIC_BING_API_KEY,
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_AUTH_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_AUTH_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_AUTH_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_AUTH_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_AUTH_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_AUTH_APP_ID,
-    NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
-    NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
-  },
   images: {
     // nextjs에서 외부경로의 이미지를 사용할수 있게 처리
     remotePatterns: [
