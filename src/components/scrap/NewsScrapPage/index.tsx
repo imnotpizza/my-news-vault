f55917814ba16e96e35090d2cad18fb983c957a9
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-06-16 21:02:03 +0900

    #41 Feat: scrap count UI 추가

--- src/components/scrap/NewsScrapPage/index.tsx
+++ src/components/scrap/NewsScrapPage/index.tsx
@@ -5,13 +5,33 @@ import React, { useContext } from 'react';
 import { userInfoContext } from '@/utils/userInfoProvider';
 import styled from 'styled-components';
 import EmptyScrapUI from '@/views/searchStatus/EmptyScrapUI';
+import ScrapCountView from '@/views/newsScrap/ScrapCountView';
+import { responsive } from '@/styles/responsive';
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
 
+  .scrap-count {
+    height: 2.5rem;
+    width: 64.63rem;
+    margin: 0 auto;
+    margin-top: 1.75rem !important;
+    color: ${(p) => p.theme.Navy.Navy_80};
+    ${responsive.tablet} {
+      width: 46.88rem;
+      margin: 0 auto;
+    }
+    ${responsive.mobile} {
+      width: 100%;
+      height: 1rem;
+      box-sizing: border-box;
+      margin: 0 1.25rem;
+    }
+  }
+
   .scrap-results {
-    margin-top: 7rem;
+    margin-top: 2rem;
     padding-bottom: 5rem;
     position: relative;
     & > div {
@@ -26,6 +46,9 @@ const NewsScrapPage = () => {
 
   return (
     <Container className="flex-column">
+      <div className="scrap-count flex-justify-start">
+        <ScrapCountView>{queryStates.data?.length}</ScrapCountView>
+      </div>
       <div className="scrap-results flex-center">
         <QueryStateWrapper
           isLoading={queryStates.isLoading}
