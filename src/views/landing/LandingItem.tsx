4a6bb6b2e18819bfe79f606d68ee78bd95ee3f76
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-06-20 14:07:22 +0900

    #42 Feat: anim effect 추가

--- src/views/landing/LandingItem.tsx
+++ src/views/landing/LandingItem.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import { styled } from 'styled-components';
+import React, { useState } from 'react';
+import useFadeUIEffect from '@/hooks/useFadeUIEffect';
+import { css, keyframes, styled } from 'styled-components';
 import LandingTitle from './LandingTitle';
 import LandingContents from './LandingContents';
 import LandingImage from './LandingImage';
-import { responsive } from '@/styles/responsive';
 
-const Container = styled.div`
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(20px);
+  };
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  };
+`;
+
+const animation = css`
+  animation: ${fadeIn} 1s ease-in-out;
+`;
+
+const Container = styled.div<{ isDetected: boolean }>`
   height: 800px;
   width: 100%;
   background-color: green;
   margin-bottom: 15rem;
+  ${(p) => p.isDetected && animation};
   .contents-box {
     width: 100%;
     height: 100%;
@@ -44,8 +60,11 @@ const LandingItem = ({
   contents: React.ReactElement | string;
   imgUrl: string;
 }) => {
+  const [isDetected, setIsDetected] = useState(false);
+  const { ref } = useFadeUIEffect(() => setIsDetected(true));
+
   return (
-    <Container className="flex-column flex-center">
+    <Container className="flex-column flex-center" ref={ref} isDetected={isDetected}>
       <div className="contents-box">
         <div className="title flex-center">
           <LandingTitle>{title}</LandingTitle>
@@ -54,10 +73,9 @@ const LandingItem = ({
           <LandingContents>{contents}</LandingContents>
         </div>
       </div>
-      <div className="image-box flex-center"><LandingImage
-        title={title}
-        imgUrl={imgUrl}
-      /></div>
+      <div className="image-box flex-center">
+        <LandingImage title={title} imgUrl={imgUrl} />
+      </div>
     </Container>
   );
 };
