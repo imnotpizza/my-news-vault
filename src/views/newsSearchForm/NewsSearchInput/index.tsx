cfed28c4b2957930bcc2fe3d6d95c20bc537c29c
imnotpizza <boooong772@naver.com>
2023-07-28 00:59:12 +0900

    [#39] Fix: input 중복폴더 1개로 변경

--- src/views/newsSearchForm/NewsSearchInput/index.tsx
+++ src/views/newsSearchForm/NewsSearchInput/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import { responsive } from '@/styles/responsive';
 import styled from 'styled-components';
 
-const Input = styled.input`
-  width: 27.69rem;
-  padding-left: 0.94rem;
-  border: 0;
+const NewsSearchInput = styled.input.attrs({ placeholder: '검색어를 입력해주세요' })`
+  width: 30.19rem;
+  height: 2.5rem;
+  border: 0.06rem solid ${(p) => p.theme.Navy.Navy_10};
   border-radius: 0.25rem;
   font-weight: 350;
   font-size: 0.81rem;
-  line-height: 1.19rem;
-  color: ${(p) => p.theme.Navy.Default};
-`;
+  padding-left: 0.94rem;
 
-const NewsSearchInput = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
-  return <Input {...props} />;
-};
+  &:hover {
+    outline: none;
+    box-shadow: 0rem 0rem 0.25rem 0.06rem ${(p) => p.theme.Blue.Blue_L};
+  }
+
+  &:focus {
+    outline: none;
+    border: 0.06rem solid ${(p) => p.theme.Blue.Blue_M};
+    box-shadow: 0rem 0rem 0.25rem 0.06rem ${(p) => p.theme.Blue.Blue_L};
+  }
+
+  ${responsive.mobile} {
+    width: 100%;
+  }
+`;
 
 export default NewsSearchInput;
