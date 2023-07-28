f21ddb6d1be6ddb3cecccd8571a7f37e2076d42d
imnotpizza <boooong772@naver.com>
2023-07-28 11:25:54 +0900

    [#39] Style: lint 적용, 안쓰는 변수  제거

--- src/views/newsCard/NewsCardProvider/NewsCardProvider.test.tsx
+++ src/views/newsCard/NewsCardProvider/NewsCardProvider.test.tsx
@@ -5,21 +5,21 @@ import NewsCardDesc from '.';
 const providerImg = '/path/to/mock-news-provider.png';
 const providerTitle = 'News Provider';
 
-const providerRegex = /\/path\/to\/mock-news-provider\.png$/;
-const defaultProviderRegex = /\/path\/to\/default-news-provider\.png$/;
-
 describe('뉴스 내용 텍스트', () => {
   test('src와 alt 속성이 올바르게 전달되었을 때 렌더링되어야 함', () => {
     render(withTestProviders(NewsCardDesc, { src: providerImg, alt: providerTitle }));
     const providerIcon = screen.getByRole('img');
     expect(providerIcon).toHaveAttribute('alt', providerTitle);
-    expect(providerIcon).toHaveAttribute('src', "/_next/image?url=%2Fpath%2Fto%2Fmock-news-provider.png&w=64&q=75");
+    expect(providerIcon).toHaveAttribute(
+      'src',
+      '/_next/image?url=%2Fpath%2Fto%2Fmock-news-provider.png&w=64&q=75',
+    );
   });
 
   test('src 없는경우, 디폴트 아이콘 보여져야 함', () => {
     render(withTestProviders(NewsCardDesc, { src: undefined, alt: providerTitle }));
     const providerIcon = screen.getByRole('img');
     expect(providerIcon).toHaveAttribute('alt', providerTitle);
-    expect(providerIcon).toHaveAttribute('src', )
+    expect(providerIcon).toHaveAttribute('src');
   });
 });
