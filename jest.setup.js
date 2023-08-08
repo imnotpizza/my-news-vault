3db2e6772c49f24bd065280e76f058856c78c8bc
imnotpizza <boooong772@naver.com>
2023-08-08 11:28:54 +0900

    Revert "[#39] Feat: beforeAll, jest.mock setup.js로 이동"
    This reverts commit ef0fb9c68f22692e04267e26b7b5b43f713f8a2f.

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,6 @@
 // Optional: configure or set up a testing framework before each test.
 // If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-import { server } from '@/msw/server';
+
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
@@ -9,35 +9,3 @@ import '@testing-library/jest-dom/extend-expect';
 if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
   // worker.start();
 }
-
-// 모든 테스트 시작 전 실행
-beforeAll(() => server.listen());
-
-// 테스트 하나 종료 후 실행
-afterEach(() => server.resetHandlers());
-
-// 모든 테스트 종료 후 실행
-afterAll(() => server.close());
-
-jest.mock('./src/firebase', () => {
-  return {
-    database: jest.fn(),
-    auth: jest.fn(),
-    googleProvider: jest.fn(),
-  };
-});
-
-jest.mock('next/router', () => ({
-  useRouter: jest.fn().mockReturnValue({
-    query: {
-      query: '테스트검색',
-    },
-  }),
-}));
-
-// @ts-ignore
-window.IntersectionObserver = jest.fn(() => ({
-  observe: () => {},
-  unobserve: () => {},
-  disconnect: () => {},
-}));
