a312f319ee21a3b668a88a3de4818ac5e6b0ca60
imnotpizza <boooong772@naver.com>
2023-08-10 21:13:24 +0900

    [#39] Fix: NewsSearchPage mockdata 개선
    - news data만 넣던것에서 type에 맞춰 전부 추가
    - TRawNewsItem 인터페이스 일부 안맞는 문제 수정

--- src/msw/handlers.ts
+++ src/msw/handlers.ts
@@ -1,3 +1,4 @@
+import { TBingNewsAPIRes } from '@/types';
 import { rest } from 'msw';
 import mockRawNewsItems from '../../mockNews.json';
 
@@ -6,15 +7,25 @@ const NEWS_COUNT_NUM = 20;
 const offset = NEWS_COUNT_NUM * 1;
 
 export const getMswRestHandler = (callback: Parameters<typeof rest.get>[1]) => {
-  return rest.get(`/news/search?mkt=en-us&q=${query}&count=${NEWS_COUNT_NUM}&offset=${offset}`, callback);
+  return rest.get(
+    `/news/search?mkt=en-us&q=${query}&count=${NEWS_COUNT_NUM}&offset=${offset}`,
+    callback,
+  );
 };
 
 // 데이터 호출
 export const handlers = [
   getMswRestHandler((req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json(mockRawNewsItems),
-    );
+    const mockData: TBingNewsAPIRes = {
+      queryContext: {
+        adultIntent: false,
+        originalQuery: 'mock',
+      },
+      readLink: 'https://api.bing.microsoft.com/api/v7/news/search?q=mock',
+      totalEstimatedMatches: 1000,
+      value: mockRawNewsItems,
+    };
+
+    return res(ctx.status(200), ctx.json(mockData));
   }),
 ];
