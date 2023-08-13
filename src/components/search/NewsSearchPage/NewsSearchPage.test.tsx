14c0e372c24f5604b154be048ababf85361afb4a
imnotpizza <boooong772@naver.com>
2023-08-13 21:13:35 +0900

    [#39] Test: msw초기화 코드 jest.setup.js로 이동

--- src/components/search/NewsSearchPage/NewsSearchPage.test.tsx
+++ src/components/search/NewsSearchPage/NewsSearchPage.test.tsx
@@ -7,15 +7,6 @@ import { queryClient } from '@/queries/queryClient';
 import { getMswRestHandler } from '@/msw/handlers';
 import NewsSearchPage from '.';
 
-// 모든 테스트 시작 전 실행
-beforeAll(() => server.listen());
-
-// 테스트 하나 종료 후 실행
-afterEach(() => server.resetHandlers());
-
-// 모든 테스트 종료 후 실행
-afterAll(() => server.close());
-
 jest.mock('../../../firebase', () => {
   return {
     database: jest.fn(),
--- src/queries/useBingNewsFetch/useBingNewsFetch.test.tsx
+++ src/queries/useBingNewsFetch/useBingNewsFetch.test.tsx
@@ -2,23 +2,6 @@ import { cleanup, fireEvent, render, screen, within } from '@testing-library/rea
 import { server } from '@/msw/server';
 import useBingNewsFetch from '.';
 
-// 모든 테스트 시작 전 실행
-beforeAll(() => server.listen());
-
-// 테스트 하나 종료 후 실행
-afterEach(() => server.resetHandlers());
-
-// 모든 테스트 종료 후 실행
-afterAll(() => server.close());
-
-jest.mock('next/router', () => ({
-  useRouter: jest.fn().mockReturnValue({
-    query: {
-      query: '테스트검색',
-    },
-  }),
-}));
-
 describe('useBingNewsFetch', () => {
   describe('enabled 테스트', () => {
     it('enabled false이면 쿼리데이터 저장 안함', () => {});
