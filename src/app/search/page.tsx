e34e26f7f57534ee5c1d4aa438b779a0ee336169
imnotpizza <48155178+imnotpizza@users.noreply.github.com>
2023-10-15 23:09:38 +0900

    Refactor: scrap, search, layout 페이지 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,52 @@
+import NewsSearchPage from '@/components/search/NewsSearchPage';
 import React from 'react';
+import { GetServerSideProps } from 'next';
+import { UserInfoProvider } from '@/utils/userInfoProvider';
+import Layout from '@/components/layout';
+import { getUserInfoFromFirebaseAdmin } from '@/utils/serverside';
+import { TPageProps } from '@/types';
+import { APP_NAME, initialPageProps } from '@/constants';
+import { prefetchScrappedNewsList } from '@/queries/useScrappedNewsList';
+import { queryClient } from '@/queries/queryClient';
+import { dehydrate } from '@tanstack/react-query';
 
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+const SearchPage = ({ userInfo, query }) => {
+  return (
+    <UserInfoProvider initialUserInfo={userInfo || null}>
+      <Layout>
+        <NewsSearchPage query={query} />
+      </Layout>
+    </UserInfoProvider>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<TPageProps> = async (context) => {
+  try {
+    // 유저 정보
+    const { authToken } = context.req.cookies;
+    const userInfo = await getUserInfoFromFirebaseAdmin(authToken);
+    // 스크랩 정보 초기화
+    if (userInfo.email) {
+      await prefetchScrappedNewsList(userInfo.email);
+    }
+    const query = (context.query.query as string) || '';
+
+    return {
+      props: {
+        ...initialPageProps,
+        userInfo,
+        query,
+        dehydratedState: dehydrate(queryClient),
+      },
+    };
+  } catch (e) {
+    return {
+      props: {
+        ...initialPageProps,
+        // errCode: e.status,
+      },
+    };
+  }
 };
 
 export default SearchPage;
