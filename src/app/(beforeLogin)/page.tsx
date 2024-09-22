c24250bfba2b76400043c2747dc947ec59457947
liam-koh <144200505+liam-koh@users.noreply.github.com>
2024-09-22 15:29:38 +0900

    Feat: metadata 관련 옵션 추가

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from 'next';
 import React from 'react';
 
 export default function HomePage() {
   return <div>this is homepage</div>;
 }
+
+export const metadata: Metadata = {
+  title: 'My News Vault',
+  description: 'news scrap',
+};
