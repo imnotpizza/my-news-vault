e81a730b786aad3f6ab2131ce8e39e2f8c61c966
liam-koh <144200505+liam-koh@users.noreply.github.com>
2024-10-01 23:05:15 +0900

    Feat: server/client component 관련 에러 수정

--- src/components/search/NewsSearchPage/index.tsx
+++ src/components/search/NewsSearchPage/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useMemo } from 'react';
 import useBingNewsFetch from '@/queries/useBingNewsFetch';
 import QueryStateWrapper from '@/components/common/QueryStateWrapper';
