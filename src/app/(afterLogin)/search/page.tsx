0264b6ce06d2f3a8e27fbc6e75f113c6007ed3c8
liam-koh <144200505+liam-koh@users.noreply.github.com>
2024-10-01 20:11:11 +0900

    Feat: 테스트용 seo 설정 추가

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,34 +1,54 @@
 import React from 'react';
-import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { prefetchScrappedNewsList } from '../../../queries/useScrappedNewsList';
 import type { Metadata, ResolvingMetadata } from 'next';
 import { fetchScrappedList } from '../../../api/client';
 import { fetchBingNews } from '../../../api/client';
 import { createMetadataObj } from '../../_lib/metadata';
+import NewsSearchPage from '@/components/search/NewsSearchPage';
 
 interface IProps {
   query: string;
 }
 
-export default async function SearchPage({query}: IProps) {
-  const queryClient = new QueryClient();
-  await prefetchScrappedNewsList('email');
-  const dehydratedState = dehydrate(queryClient);
+export default async function SearchPage({ query }: IProps) {
+  // const queryClient = new QueryClient();
+  // await prefetchScrappedNewsList('email');
+  // const dehydratedState = dehydrate(queryClient);
 
-  return (
-    <HydrationBoundary state={dehydratedState}>
-      {/* TODO: search 컴포넌트 작성 */}
-    </HydrationBoundary>
-  )
+  return <NewsSearchPage query={query} />;
+  // return (
+  //   <HydrationBoundary state={dehydratedState}>
+  //     {/* TODO: search 컴포넌트 작성 */}
+  //   </HydrationBoundary>
+  // )
 }
 
 type Props = {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export async function generateMetadata({params, searchParams}: Props): Promise<Metadata> {
+export async function generateMetadata({ params, searchParams }: Props): Promise<Metadata> {
   const res = await fetchBingNews('', 0);
+  const seoVals = res.value.map((item) => {
+    return {
+      name: item.name,
+      description: item.description,
+      image: item.image?.thumbnail?.contentUrl,
+    };
+  });
+  const first = seoVals[0];
+  // 제목, 내용, 썸내일
+
   // TODO 여기에 seo 데이터 초기화
-  // return createMetadataObj({});
+  return createMetadataObj({
+    title: first.name,
+    description: first.description,
+    openGraph: {
+      title: first.name,
+      description: first.description,
+      images: [{ url: first.image }],
+    },
+  });
 }
