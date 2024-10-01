f6eabe645a88e6292d94204d164f24b04adc8bb7
liam-koh <144200505+liam-koh@users.noreply.github.com>
2024-10-01 19:10:21 +0900

    ADD: layout에 children 추가

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function AfterLoginLayout() {
-  return <div></div>;
+export default function AfterLoginLayout({ children }) {
+  return <div>{children}</div>;
 }
