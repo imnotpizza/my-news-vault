829e024d70d635a1a87258675e30f2ba19fb9caf
liam-koh <144200505+liam-koh@users.noreply.github.com>
2024-10-04 12:14:37 +0900

    FIX: type 에러 수정
    - styled-components 컴포넌트 사용시 타입에러 발생, 관련 의존성 최신으로 업데이트하여 해결

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { prefetchScrappedNewsList } from '../../../queries/useScrappedNewsList';
-import type { Metadata, ResolvingMetadata } from 'next';
-import { fetchScrappedList } from '../../../api/client';
-import { fetchBingNews } from '../../../api/client';
-import { createMetadataObj } from '../../_lib/metadata';
-import NewsSearchPage from '@/components/search/NewsSearchPage';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import type { Metadata } from 'next';
 import { queryClient } from '@/queries/queryClient';
-
+import NewsSearchPage from '@/components/search/NewsSearchPage';
+import { fetchBingNews } from '@/api/client';
+import { createMetadataObj } from '@/app/_lib/metadata';
+import { prefetchScrappedNewsList } from '@/queries/useScrappedNewsList';
 interface IProps {
   query: string;
 }
@@ -16,11 +14,10 @@ export default async function SearchPage(props: IProps) {
   // TODO: prefetchQuery or getQueryData 호출
   await prefetchScrappedNewsList('email');
   const dehydratedState = dehydrate(queryClient);
-
   return (
-    <HydrationBoundary state={dehydratedState}>
-      {/* TODO: search 컴포넌트 작성 */}
-      <NewsSearchPage query={props.query} />;
+    // @ts-ignore
+    <HydrationBoundary queryClient={queryClient} state={dehydratedState}>
+      <NewsSearchPage query={props.query} />
     </HydrationBoundary>
   );
 }
@@ -41,7 +38,6 @@ export async function generateMetadata({ params, searchParams }: Props): Promise
     };
   });
   const first = seoVals[0];
-  // 제목, 내용, 썸내일
 
   // TODO 여기에 seo 데이터 초기화
   return createMetadataObj({
