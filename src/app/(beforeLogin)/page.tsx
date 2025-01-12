354ed4a1107a34ee24256e67e68d53df2c1e1faa
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-01-12 19:16:55 +0900

    Style: unused import 제거

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,25 +1,6 @@
 import { Metadata } from 'next';
 import React from 'react';
 import { createMetadataObj } from '../_lib/metadata';
-import HomePage from './_component/HomePage';
-import { Button } from '@/components/atoms/Button';
-import { toast } from '@/hooks/useToast';
-import Card from '@/components/atoms/CardUI';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/atoms/SelectBox';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/atoms/Dialog';
 import UITest from '@/components/templates/UITest';
 
 //  <HomePage />
