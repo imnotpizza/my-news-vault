9df91bcc2f2144c509e8e2bca00de17a4920210f
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-01-12 18:21:05 +0900

    Test: 버튼 단위테스트 추가
    - 가장 기본적인 것만 추가, 필요하면 더추가예정

---
+++ src/components/atoms/Button/Button.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import { Button } from '.';
+
+describe('Button', () => {
+  it('should render correctly', () => {
+    render(<Button>Click me</Button>);
+    expect(screen.getByText('Click me')).toBeInTheDocument();
+  });
+});
