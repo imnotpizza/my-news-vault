afd82d03d2411ebade27dc0bc4398bc6a8c521cf
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-01-26 00:21:21 +0900

    Fix: 언론사 프로필위치 변경

--- src/components/organisms/search/NewsCard/index.tsx
+++ src/components/organisms/search/NewsCard/index.tsx
@@ -17,15 +17,15 @@ function NewsCard({ newsItem }: IProps) {
   const { title, description, url, providerIcon, datePublished, thumbnail } = newsItem;
   return (
     <Card className="p-0 w-[14.44rem] h-[23.38rem] relative">
+      <ProfileImage
+        src={providerIcon}
+        alt={title}
+        className="absolute top-[0.6rem] left-[0.6rem] w-[2.5rem] h-[2.5rem]"
+      />
       {/* thumbnail */}
       <img src={thumbnail} alt={title} className="w-full" />
-      <Card.Content className="p-4 relative">
-        <ProfileImage
-          src={providerIcon}
-          alt={title}
-          className="absolute top-[-1.2rem] left-[40%] w-[2.5rem] h-[2.5rem]"
-        />
-        <Card.Title className="text-md mt-3">{title}</Card.Title>
+      <Card.Content className="p-4">
+        <Card.Title className="text-md">{title}</Card.Title>
         <Card.Description className="text-sm">{description}</Card.Description>
       </Card.Content>
       <Card.Footer className="absolute bottom-0 left-0 w-[14.44rem] flex justify-between p-4">
