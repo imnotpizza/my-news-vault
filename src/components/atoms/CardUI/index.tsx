bdd033521185505bddabab5245d97fb71eb76d14
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-01-25 16:21:56 +0900

    Feat: card ui 피그마에 맞게 스타일 수정
    - border 색상 지정

--- src/components/atoms/CardUI/index.tsx
+++ src/components/atoms/CardUI/index.tsx
@@ -26,7 +26,10 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
   ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={cn('rounded-lg border bg-card text-card-foreground shadow-sm', className)}
+      className={cn(
+        'rounded-lg border border-solid border-mnv-gray-10 bg-card text-card-foreground shadow-md',
+        className,
+      )}
       {...props}
     />
   ),
