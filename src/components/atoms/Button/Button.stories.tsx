89319a44d3ad65c98d1dae64dd00fd332a5480ee
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-01-25 15:55:07 +0900

    Feat: button 옵션별 스토리 추가

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -24,3 +24,37 @@ Default.args = {
   variant: 'default',
   size: 'default',
 };
+
+export const ButtonsByVariant = () => {
+  return (
+    <div className="flex gap-2">
+      <Button size="default" variant="default">
+        default
+      </Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="link">link</Button>
+    </div>
+  );
+};
+
+export const ButtonsBySize = () => {
+  return (
+    <div className="flex gap-2">
+      <Button size="default">default</Button>
+      <Button size="sm">sm</Button>
+      <Button size="lg">lg</Button>
+      <Button size="icon">icon</Button>
+    </div>
+  );
+};
+
+export const ButtonsByAsChild = () => {
+  return (
+    <Button asChild variant="link">
+      <a href="#">Link</a> 
+    </Button>
+  );
+};
