1a7c3b9ac82fe5719dad4120e3da841d52414d03
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-01-26 01:47:41 +0900

    Feat: haeder 추가

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Layout from '@/components/atoms/Layout';
+import Header from '@/components/templates/common/Header';
 
+/**
+ * 로그인 상태에서의 layout
+ */
 export default function AfterLoginLayout({ children }) {
-  return <div>{children}</div>;
+  return (
+    <Layout className="h-auto">
+      <Layout.Header>
+        <Header />
+      </Layout.Header>
+      <Layout.Body>{children}</Layout.Body>
+    </Layout>
+  );
 }
