43909a11b1929402c742315beedd730e748c0ab2
liam-koh <48155178+imnotpizza@users.noreply.github.com>
2025-02-11 00:32:15 +0900

    Refactor: 관련 코드 정리

--- src/components/molecules/LoginButton/index.tsx
+++ src/components/molecules/LoginButton/index.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import { Button } from '@/components/atoms/Button';
+import { useSignin } from '@/queries/useUserAuth';
 // import LoginButtonIcon from '@/assets/svgs/login-button-icon.svg';
-import React from 'react';
+import { userInfoContext } from '@/utils/userInfoProvider';
+import React, { useContext } from 'react';
 
 interface IProps extends React.HTMLAttributes<HTMLButtonElement> {}
 
@@ -8,8 +12,20 @@ interface IProps extends React.HTMLAttributes<HTMLButtonElement> {}
  * 로그인 버튼, 버튼 클릭시 로그인 수행
  */
 export default function LoginButton({ ...props }: IProps) {
+  const { setUserInfo } = useContext(userInfoContext);
+  const { mutate } = useSignin({
+    onSuccess: (newUserInfo) => {
+      setUserInfo(() => newUserInfo);
+      window.location.reload();
+    },
+  });
+
+  const onClickSignin = () => {
+    mutate();
+  };
+
   return (
-    <Button variant="outline" {...props}>
+    <Button variant="outline" {...props} onClick={onClickSignin}>
       <div className="flex gap-2 items-center">
         {/* FIXME: svg 로드시 에러발생 */}
         {/* <LoginButtonIcon /> */}
--- src/utils/userInfoProvider.tsx
+++ src/utils/userInfoProvider.tsx
@@ -1,28 +1,27 @@
-// import { TUserInfo } from '@/types';
-// import React from 'react';
+import { TUserInfo } from '@/types';
+import React from 'react';
 
-// export interface IUserInfoContext {
-//   userInfo: TUserInfo | null;
-//   setUserInfo: React.Dispatch<React.SetStateAction<TUserInfo | null>>;
-//   isSignin: boolean;
-// }
+export interface IUserInfoContext {
+  userInfo: TUserInfo | null;
+  setUserInfo: React.Dispatch<React.SetStateAction<TUserInfo | null>>;
+  isSignin: boolean;
+}
 
-// const userInfoContext = React.createContext<IUserInfoContext>({
-//   userInfo: null,
-//   setUserInfo: () => {},
-//   isSignin: false,
-// });
+const userInfoContext = React.createContext<IUserInfoContext>({
+  userInfo: null,
+  setUserInfo: () => {},
+  isSignin: false,
+});
 
-// const UserInfoProvider = ({ children, initialUserInfo = null }: any) => {
-//   const [userInfo, setUserInfo] = React.useState<IUserInfoContext['userInfo']>(initialUserInfo);
+const UserInfoProvider = ({ children, initialUserInfo = null }: any) => {
+  const [userInfo, setUserInfo] =
+    React.useState<IUserInfoContext['userInfo']>(initialUserInfo);
 
-//   return (
-//     <userInfoContext.Provider value={{ userInfo, setUserInfo, isSignin: Boolean(userInfo) }}>
-//       {children}
-//     </userInfoContext.Provider>
-//   );
-// };
+  return (
+    <userInfoContext.Provider value={{ userInfo, setUserInfo, isSignin: Boolean(userInfo) }}>
+      {children}
+    </userInfoContext.Provider>
+  );
+};
 
-// export { userInfoContext, UserInfoProvider };
-
-export {};
+export { userInfoContext, UserInfoProvider };
