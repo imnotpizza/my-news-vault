5a649531e3b09ef7ce89982009f87c8b76dc7413
liam-koh <48155178+imnotpizza@users.noreply.github.com>
2025-02-14 22:24:22 +0900

    Fix: useMutation v5형식으로 업데이트

--- src/queries/useUserAuth.tsx
+++ src/queries/useUserAuth.tsx
@@ -2,9 +2,9 @@ import { TUserInfo } from '@/types';
 import { UseMutationOptions, useMutation } from '@tanstack/react-query';
 
 export const useSignin = (options: UseMutationOptions<TUserInfo>) => {
-  const queryStates = useMutation<TUserInfo, Error>(
-    // @ts-ignore
-    async () => {
+  const queryStates = useMutation({
+    mutationKey: ['signin'],
+    mutationFn: async () => {
       const newUserInfo = 'user';
       // @ts-ignore
       if (!newUserInfo) {
@@ -12,18 +12,19 @@ export const useSignin = (options: UseMutationOptions<TUserInfo>) => {
       }
       return newUserInfo;
     },
-    {
-      ...options,
-      onError: () => {
-        alert('로그인 도중 문제가 발생하였습니다.');
-      },
+    onError: () => {
+      alert('로그인 도중 문제가 발생하였습니다.');
     },
-  );
+    ...options,
+  });
   return queryStates;
 };
 
 export const useSignout = (options: UseMutationOptions<void>) => {
   // @ts-ignore
-  const queryStates = useMutation<void, Error>(async () => {}, options);
+  const queryStates = useMutation({
+    mutationFn: async () => {},
+    ...options,
+  });
   return queryStates;
 };
