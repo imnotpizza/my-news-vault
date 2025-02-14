7a883ea3805680966ddc22170b83868273f99b92
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-02-14 18:02:10 +0900

    RFT: handler 함수 별도 분리 후 공용

--- src/components/etc/MSWTest.stories.tsx
+++ src/components/etc/MSWTest.stories.tsx
@@ -3,6 +3,7 @@ import MSWTest from './MSWTest';
 import { Meta } from '@storybook/react';
 import RootProviders from './RootProviders';
 import { http, HttpResponse } from 'msw';
+import { mswTestHandler } from '../../../mocks/handler/newsHandler';
 
 export default {
   title: 'Atoms/MSWTest',
@@ -21,14 +22,6 @@ export const MswEmpty = Template.bind({});
 
 MswEmpty.parameters = {
   msw: {
-    handlers: [
-      http.get('https://jsonplaceholder.typicode.com/posts', ({ params }) => {
-        const mockPosts = [
-          { userId: 1, id: 1, title: '테스트 글 제목 #1', body: '내용 예시 #1' },
-          { userId: 2, id: 2, title: '테스트 글 제목 #2', body: '내용 예시 #2' },
-        ];
-        return HttpResponse.json(mockPosts);
-      }),
-    ],
+    handlers: [...mswTestHandler],
   },
 };
