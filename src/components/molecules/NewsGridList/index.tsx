96c5003109c37330e8af48104e8580c0b5f74845
liam-koh <48155178+imnotpizza@users.noreply.github.com>
2025-03-01 01:53:13 +0900

    ADD: grid width 고정

--- src/components/molecules/NewsGridList/index.tsx
+++ src/components/molecules/NewsGridList/index.tsx
@@ -10,7 +10,7 @@ interface IProps {
  */
 export default function NewsGridList({ children }: IProps) {
   return (
-    <div className="flex flex-col items-center w-full min-h-screen">
+    <div className="flex flex-col items-center w-full min-h-screen desktop:w-[64rem] tablet:w-[48rem]">
       <div className="grid grid-cols-1 gap-5 w-full max-w-5xl px-5 mb-7 tablet:grid-cols-3 desktop:grid-cols-4 mobile:grid-cols-1">
         {children}
       </div>
