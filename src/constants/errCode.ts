8f37763c0695cbf93b601b6e7550446d8dd86861
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-03-10 17:25:54 +0900

    Feat: 에러코드 as const 로 작성, 주석 추가

--- src/constants/errCode.ts
+++ src/constants/errCode.ts
@@ -1,4 +1,8 @@
-/** ERROR CODE */
+/** 
+ * 에러코드
+ * AUTH: 사용자 로그인/로그아웃 관련
+ * API: API 호출 관련 (BingAPI, Scrap 등등)
+ */
 const ERRCODE = {
   AUTH: {
     ALREADY_LOGINED: 10001,
@@ -12,10 +16,16 @@ const ERRCODE = {
       SCRAP_FAILED: 11003,
       UNSCRAP_FAILED: 11004,
     },
+    SCRAP: {
+      FETCH_FAILED: 12001,
+      ADD_FAILED: 12002,
+      DELETE_FAILED: 12003,
+    }
   },
   NETWORK_ERROR: 20000,
   TIMEOUT_ERROR: 20001,
   UNKNOWN: 99999,
-};
+} as const;
+type ERRCODE = (typeof ERRCODE)[keyof typeof ERRCODE];
 
 export default ERRCODE;
