91a7f0515c2468e44a083443bb1d456f1d9ecf94
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-03-11 18:24:43 +0900

    Feat: error boundary에서 Errcode에 따라 UI/toast 출력 변경 추가

--- src/components/atoms/ErrorBoundary/index.tsx
+++ src/components/atoms/ErrorBoundary/index.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import ERRCODE from '@/constants/errCode';
 import { useToast } from '@/hooks/useToast';
+import APIError from '@/utils/APIError';
 import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import React from 'react';
 import { ErrorBoundary as EBoundary } from 'react-error-boundary';
 import { twMerge } from 'tailwind-merge';
+import SearchErrorIcon from '@/assets/search-error-icon.svg';
+import SearchNoneIcon from '@/assets/search-none-icon.svg';
 
 interface IProps {
   fallbackContent?: (props: { resetErrorBoundary: () => void }) => React.ReactElement;
@@ -17,11 +22,12 @@ interface IProps {
  * react-query ErrorBoundary Wrapper 컴포넌트
  * @param {function} fallbackRender - retry 실행기능 포함된 컴포넌트 리턴 함수
  * @param {React.ReactNode} children - 에러 감쌀 컴포넌트
+ * TODO: 서비스별로 UI / fallback 등 분리
  */
 export default function ErrorBoundary({
   fallbackContent = () => (
     <div role="status" className="w-full h-[70vh] flex justify-center items-center grow">
-      {/* 일시적인 문제가 발생하였습니다 */}
+      일시적인 문제가 발생하였습니다
     </div>
   ),
   onError,
@@ -29,6 +35,22 @@ export default function ErrorBoundary({
   className,
 }: IProps) {
   const { toast } = useToast();
+  const [fallbackIcon, setFallbackIcon] = React.useState<React.ReactNode | null>(null);
+
+  const setFallbackIconByErrCode = (errCode: ERRCODE) => {
+    switch (errCode) {
+      case ERRCODE.NEWS_FETCH_FAILED:
+        setFallbackIcon(SearchErrorIcon);
+        break;
+      case ERRCODE.NEWS_FETCH_NOT_FOUND:
+        setFallbackIcon(SearchNoneIcon);
+        break;
+      default:
+        setFallbackIcon(SearchErrorIcon);
+        break;
+    }
+  };
+
   return (
     <QueryErrorResetBoundary>
       {({ reset }) => (
@@ -36,14 +58,20 @@ export default function ErrorBoundary({
           onReset={reset}
           fallbackRender={(props) => (
             <div className={twMerge('w-full h-full flex-center', className)}>
-              {fallbackContent(props)}
+              {fallbackIcon || '일시적인 문제가 발생하였습니다.'}
             </div>
           )}
-          onError={() => {
-            // TODO: 지역 에러처리는 여기서
-            toast({
-              description: '데이터를 불러오는 중 문제가 발생했습니다.',
-            });
+          onError={(e) => {
+            if (e instanceof APIError) {
+              setFallbackIconByErrCode(e.errCode);
+              toast({
+                description: e.msgFromCode,
+              });
+            } else {
+              toast({
+                description: '일시적인 문제가 발생하였습니다',
+              });
+            }
           }}
         >
           {children}
