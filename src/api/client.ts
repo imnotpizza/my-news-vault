169918c6a29a30a169e7c189890489e12bd524db
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-03-11 17:27:56 +0900

    Feat: try/catch 추가 후 커스텀 에러 인스턴스 throw 구문 추가

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,6 +2,8 @@ import { TBingNewsAPIRes, TBingNewsFilterQueries, TNewsItem, TUserInfo } from '@
 import { doc, getDocs, collection, deleteDoc, setDoc } from 'firebase/firestore/lite';
 import { database } from '@/firebase';
 import BingAPI from '@/api/BingAPI';
+import APIError from '@/utils/APIError';
+import ERRCODE from '@/constants/errCode';
 
 // 한번에 몇개씩 호출할지 결정
 const NEWS_COUNT_NUM = 20;
@@ -16,38 +18,54 @@ export const fetchBingNews = async (
   query: TBingNewsFilterQueries['keyword'],
   pageNum: number,
 ) => {
-  const offset = NEWS_COUNT_NUM * pageNum;
-  const url = `news/search?mkt=en-us&q=${query}&count=${NEWS_COUNT_NUM}&offset=${offset}`;
-  const apiRes = await BingAPI.get<TBingNewsAPIRes>(url);
-  return apiRes.data;
+  try {
+    const offset = NEWS_COUNT_NUM * pageNum;
+    const url = `news/search?mkt=en-us&q=${query}&count=${NEWS_COUNT_NUM}&offset=${offset}`;
+    const apiRes = await BingAPI.get<TBingNewsAPIRes>(url);
+    return apiRes.data;
+  } catch (e) {
+    throw new APIError(ERRCODE.NEWS_FETCH_FAILED);
+  }
 };
 
 /**
  * 스크랩된 데이터 호출
  */
 export const fetchScrappedList = async (userId: TUserInfo['email']) => {
-  const path = `scrap/${userId}/scrap`;
-  const res: TNewsItem[] = [];
-  const snapshot = await getDocs(collection(database, path));
-  snapshot.forEach((_doc) => {
-    const data = _doc.data() as TNewsItem;
-    data.isScrapped = true;
-    res.push(data);
-  });
-  return res;
+  try {
+    const path = `scrap/${userId}/scrap`;
+    const res: TNewsItem[] = [];
+    const snapshot = await getDocs(collection(database, path));
+    snapshot.forEach((_doc) => {
+      const data = _doc.data() as TNewsItem;
+      data.isScrapped = true;
+      res.push(data);
+    });
+    return res;
+  } catch (e) {
+    throw new APIError(ERRCODE.SCRAP_FETCH_FAILED);
+  }
 };
 
 /**
  * 스크랩
  */
 export const scrapNews = async (userId: TUserInfo['email'], newsItem: TNewsItem) => {
-  await setDoc(doc(database, `scrap/${userId}/scrap`, newsItem.newsId), newsItem);
+  try {
+    await setDoc(doc(database, `scrap/${userId}/scrap`, newsItem.newsId), newsItem);
+  } catch (e) {
+    throw new APIError(ERRCODE.SCRAP_ADD_FAILED);
+  }
 };
 
 /**
  * 스크랩 해제
  */
 export const unscrapNews = async (userId: TUserInfo['email'], newsId: string) => {
-  const target = doc(database, `scrap/${userId}/scrap`, newsId);
-  await deleteDoc(target);
+  try {
+    const target = doc(database, `scrap/${userId}/scrap`, newsId);
+    await deleteDoc(target);
+  } catch (e) {
+    throw new APIError(ERRCODE.SCRAP_DELETE_FAILED);
+  }
 };
