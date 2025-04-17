5e17a395754f789f9c4fa7e4a5a4131e528943be
liam-koh <liam@o2pluss.com>
2025-04-17 15:31:07 +0900

    Feat: google 로그인 개선 및 아이콘 추가
    https://developers.google.com/identity/branding-guidelines

--- src/components/molecules/LoginButton/index.tsx
+++ src/components/molecules/LoginButton/index.tsx
@@ -8,6 +8,7 @@ import { authAtom, userInfoContext } from '@/utils/userInfoProvider';
 import { useAtom } from 'jotai';
 import { useRouter } from 'next/navigation';
 import React, { useContext } from 'react';
+import GoogleIcon from '@/assets/google.svg';
 
 interface IProps extends React.HTMLAttributes<HTMLButtonElement> {}
 
@@ -30,11 +31,17 @@ export default function LoginButton({ ...props }: IProps) {
   };
 
   return (
-    <Button variant="outline" {...props} onClick={onClickSignin}>
-      <div className="flex gap-2 items-center">
-        {/* FIXME: svg 로드시 에러발생 */}
-        {/* <LoginButtonIcon /> */}
-        <span>로그인</span>
+    <Button
+      variant="ghost"
+      {...props}
+      onClick={onClickSignin}
+      className="relative flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md shadow hover:shadow-md transition duration-150"
+    >
+      <div className="absolute inset-0 bg-black opacity-0 hover:opacity-5 transition-opacity rounded-md pointer-events-none"></div>
+      <div className="flex items-center space-x-3 relative z-10">
+        <GoogleIcon className="w-5 h-5" />
+        <span className="text-sm font-medium text-gray-700">Sign in with Google</span>
+        <span className="sr-only">Sign in with Google</span>
       </div>
     </Button>
   );
