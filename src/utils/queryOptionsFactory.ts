3a2d8e669bc9bd9dc8abf90fd51284fae13f3a94
liam-koh <liam@o2pluss.com>
2025-04-18 11:18:11 +0900

    ADD: 필요한 query key 및 query options들 추가

--- src/utils/queryOptionsFactory.ts
+++ src/utils/queryOptionsFactory.ts
@@ -12,6 +12,18 @@ import {
   setIsScrapped,
 } from './newsItem';
 
+// private queries (인증없이 호출 불가F)
+export const QK_PRIVATE = 'PRIVATE';
+// public queries (인증없이도 호출가능)
+export const QK_PUBLIC = 'PUBLIC';
+
+/** query key: 뉴스 api */
+export const QK_NEWS = 'NEWS';
+/** query key: bing news */
+export const QK_BING = 'BING';
+/** query key: scrap api */
+export const QK_SCRAP = 'SCRAP';
+
 /**
  * queryKey, queryFn을 이용하여 쿼리 옵션을 생성하는 팩토리 함수
  * useQuery Hook, queryClient fn에 다음 옵션 넣어 사용
@@ -21,7 +33,7 @@ const queryOptionsFactory = () => ({
   news: {
     bing: {
       list: (filterQueries: TBingNewsFilterQueries) => ({
-        queryKey: [{ ...filterQueries }],
+        queryKey: [QK_PUBLIC, QK_NEWS, QK_BING, 'LIST', { ...filterQueries }],
         // FIXME: 나중에 api결과 처리하는 부분 어디에 둘지 결정 필요(전부 애매함)
         queryFn: async ({ pageParam = 1 }) => {
           const { keyword } = filterQueries;
@@ -59,9 +71,9 @@ const queryOptionsFactory = () => ({
   },
   // scrap api
   scrap: {
-    list: {
-      queryKey: null,
-      queryFn: (userId: TUserInfo['email']) => fetchScrappedList(userId),
-    },
+    list: (userId: TUserInfo['email']) => ({
+      queryKey: [QK_PRIVATE, QK_SCRAP, 'LIST', userId],
+      queryFn: () => fetchScrappedList(userId),
+    }),
   },
 });
