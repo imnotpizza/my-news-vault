d24caab3433a6a2d4640370ba93c76fe3e9e98f1
liam-koh <48155178+imnotpizza@users.noreply.github.com>
2025-05-03 00:00:00 +0900

    Feat: 랜딩페이지 리뉴얼 기본 레이아웃 및 소개글 추가
    - 봐서 내용도 몇개 추가

--- src/app/(beforeLogin)/_component/HomePage.tsx
+++ src/app/(beforeLogin)/_component/HomePage.tsx
@@ -1,76 +1,52 @@
-'use client';
-
-import { APP_NAME } from '@/constants';
-import LandingContents from '@/views/landing/LandingContents';
-import LandingItem from '@/views/landing/LandingItem';
-import ToSearchPageButton from '@/views/landing/ToSearchPageButton';
-import Image from 'next/image';
+import { Button } from '@/components/atoms/Button';
+import Link from 'next/link';
 import React from 'react';
 
-function HomePage() {
+export default function HomePage() {
   return (
-    <div className="w-full min-h-[1000px] mx-auto px-4 md:min-h-[800px] md:w-9/10">
-      <div className="flex flex-col items-center justify-center w-full h-[900px]">
-        <div className="mb-12 w-[500px] h-[130px] md:w-4/5">
-          <Image
-            width={500}
-            height={200}
-            src="/svg/landing-icon.svg"
-            alt="my news vault"
-            className="w-full h-full"
-          />
-        </div>
-        <div className="mb-12">
-          <LandingContents>스크랩한 뉴스를 통해 나만의 뉴스를 만들어보세요.</LandingContents>
-        </div>
-        <div>
-          <ToSearchPageButton />
-        </div>
-      </div>
-      <div className="landing-items">
-        <div className="mb-[500px] md:mb-[300px]">
-          <LandingItem
-            title="편리한 검색 기능"
-            contents={
-              <p>
-                지금 당신이 원하는 뉴스, 검색만으로 끝!
-                <br /> 새로운 정보의 세계로 떠나보세요. <br /> {APP_NAME}, 최고의 뉴스 검색
-                사이트입니다.
+    <main className="bg-gradient-to-b from-primary/80 to-primary/20 w-screen h-screen">
+      <section className="py-10 sm:py-16 lg:py-24">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
+            <div>
+              {/* 좌->우 애니메이션 */}
+              <div className="flex flex-col gap-4">
+                <h1 className="text-4xl font-bold text-black sm:text-6xl lg:text-7xl">
+                  <span className="">My News Vault</span>
+                </h1>
+                <div className="relative inline-flex">
+                  <span className="absolute inset-x-0 bottom-0 border-b-[30px]"></span>
+                  {/* 좌-우 형광펜 애니메이션 */}
+                  <h3 className="relative text-2xl font-bold text-black sm:text-4xl lg:text-4xl bg-[#4ADE80]">
+                    빠르게 만나는 신선한 뉴스
+                  </h3>
+                </div>
+              </div>
+              {/* 좌->우 애니메이션 */}
+              <p className="mt-8 text-base text-black sm:text-xl">
+                My News Vault는 원하는 뉴스를 쉽고 빠르게 검색하고, 중요한 기사들을 안전하게
+                스크랩하여 보관할 수 있는 뉴스 관리 앱입니다.
+                <br /> 키워드 기반 검색, 기사 즐겨찾기, 맞춤형 뉴스 알림 기능을 통해 나만의
+                뉴스 아카이브를 만들어보세요.
               </p>
-            }
-            imgUrl="/gif/landing-page-svg1.gif"
-          />
+              {/* 좌->우 애니메이션 */}
+              <div className="mt-10 sm:flex sm:items-center sm:space-x-8">
+                <Link href="/search">
+                  <Button>My News Vault 시작하기</Button>
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <img
+                className="w-full"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/2/hero-img.png"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
-        <div className="mb-[500px] md:mb-[300px]">
-          <LandingItem
-            title="좋아하는 뉴스 스크랩"
-            contents={
-              <p>
-                뉴스 스크랩, 클릭 한 번으로 모아보세요!
-                <br /> {APP_NAME}이 제공하는 간편한 스크랩 기능으로 <br />
-                소중한 뉴스를 저장하고 언제든지 다시 확인해보세요.
-              </p>
-            }
-            imgUrl="/gif/landing-page-svg2.gif"
-          />
-        </div>
-        <div className="mb-[150px] md:mb-[100px]">
-          <LandingItem
-            title="스크랩 목록 한번에 확인"
-            contents={
-              <p>
-                손쉽게 정보를 확인하세요.
-                <br /> 스크랩 목록에서 관심있는 뉴스를 확인하고,
-                <br /> 소중한 정보를 쉽게 찾아보세요.
-              </p>
-            }
-            imgUrl="/gif/landing-page-svg3.gif"
-          />
-        </div>
-      </div>
-      <div className="w-full h-px" />
-    </div>
+      </section>
+    </main>
   );
 }
-
-export default HomePage;
