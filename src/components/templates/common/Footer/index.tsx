1f6df60deb8b29fe3a165590f2795d4de947c34b
liam-koh <48155178+imnotpizza@users.noreply.github.com>
2025-05-07 21:10:30 +0900

    Rft: route group 수정
    - 임시명칭 대신 landingPageGroup, servicePageGroup으로 변경

--- src/components/templates/common/Footer/index.tsx
+++ src/components/templates/common/Footer/index.tsx
@@ -1,3 +1,4 @@
+import Layout from '@/components/atoms/Layout';
 import React from 'react';
 
 /**
@@ -6,29 +7,31 @@ import React from 'react';
  */
 export default function FooterTemplate() {
   return (
-    <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 text-center">
-      <p className="text-sm text-gray-600">
-        ⓒ {new Date().getFullYear()} My News Vault. All rights reserved.
-      </p>
-      <div className="mt-2 flex justify-center gap-4 text-sm text-gray-600">
-        <a
-          href="mailto:bobin6972@gmail.com"
-          className="hover:underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          📧 bobin6972@gmail.com
-        </a>
-        <span>|</span>
-        <a
-          href="https://github.com/imnotpizza"
-          className="hover:underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          💻 GitHub
-        </a>
+    <Layout.Footer className="py-10 bg-primary/10 border-t border-gray-200">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 text-center">
+        <p className="text-sm text-gray-600">
+          ⓒ {new Date().getFullYear()} My News Vault. All rights reserved.
+        </p>
+        <div className="mt-2 flex justify-center gap-4 text-sm text-gray-600">
+          <a
+            href="mailto:bobin6972@gmail.com"
+            className="hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            📧 bobin6972@gmail.com
+          </a>
+          <span>|</span>
+          <a
+            href="https://github.com/imnotpizza"
+            className="hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            💻 GitHub
+          </a>
+        </div>
       </div>
-    </div>
+    </Layout.Footer>
   );
 }
