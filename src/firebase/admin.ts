78cc78ca32387754bc30813a55a3ea41824d47ae
IMNOTPIZZA <48155178+imnotpizza@users.noreply.github.com>
2025-08-05 01:07:31 +0900

    재변경

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -4,7 +4,7 @@ const config = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   //@ts-ignore
-  privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY.replace(/\n/gm, '\n'),
+  privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/gm, "\n"),
   clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
 };
 if (!admin.apps.length) {
