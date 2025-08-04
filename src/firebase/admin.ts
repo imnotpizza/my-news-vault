f451fb234118f043de8c71eed867f8bd5646a66d
IMNOTPIZZA <48155178+imnotpizza@users.noreply.github.com>
2025-08-05 01:09:19 +0900

    Update admin.ts

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -4,7 +4,8 @@ const config = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   //@ts-ignore
-  privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/gm, "\n"),
+  // privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/gm, "\n"),
+  privateKey: Buffer.from(process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY).toString(),
   clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
 };
 if (!admin.apps.length) {
