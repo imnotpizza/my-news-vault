c352de045dcb4b9614527c7e4c65aac079a0c820
IMNOTPIZZA <48155178+imnotpizza@users.noreply.github.com>
2025-08-05 07:13:35 +0900

    임시 제거

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -3,9 +3,10 @@ import * as admin from 'firebase-admin';
 const config = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  privateKey: '',
   //@ts-ignore
   // privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/gm, "\n"),
-  privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY ? process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+  // privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY ? process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
   clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
 };
 if (!admin.apps.length) {
