b5f2acb3370f5cae1bc34ebabc8b24c50ca366b0
IMNOTPIZZA <48155178+imnotpizza@users.noreply.github.com>
2025-08-06 00:29:53 +0900

    standalone 배포 구축

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       name: 'mnv-prod', // pm2 instance 이름
       // script args 로 명령어 실행
       script: 'yarn', // yarn 혹은 npm 혹은 다른 것도 가능
-      args: 'start', // 실행할 scripts
+      args: 'start:st', // 실행할 scripts
       exec_mode: 'cluster', // fork 혹은 cluster, 어떤 모드인지는 공식 문서 참조
       interpreter: 'bash', // next.js와 pm2 사용시 interpreter 설정
       watch: 'true', // 파일 변경 감지
